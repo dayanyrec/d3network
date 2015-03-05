b76e86ecf5faf0111cedd543305aac9dbb4c5fe4
Dayany Espindola <dayanyrec@gmail.com>
2015-03-05 18:24:56 -0300

    Render nodes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 	var svg,
 		width = window.innerWidth,
-		height = window.innerHeight - document.querySelector("h2").offsetHeight,
+		height = window.innerHeight - document.querySelector('h2').offsetHeight,
 		node;
 
 	svg = d3.select('body')
@@ -12,4 +12,19 @@ window.onload = function () {
 		.append('svg')
 		.attr('width', width)
 		.attr('height', height);
+
+	node = svg.append('g')
+		.attr('class', 'node')
+		.selectAll('circle');
+
+	d3.json('data.json', function (error, data) {
+		node = node.data(data.nodes).enter().append('circle')
+			.attr('r', 4)
+			.attr('cx', function (d) {
+				return d.x;
+			})
+			.attr('cy', function (d) {
+				return d.y;
+			});
+	});
 };
